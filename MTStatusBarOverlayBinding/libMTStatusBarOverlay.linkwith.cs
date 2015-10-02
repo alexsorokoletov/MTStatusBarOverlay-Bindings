7e17da205b66deb53c6bbfb8866d8f45ef4fe657
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2015-10-02 18:10:21 -0400

    .a file updated to support iOS 9 and x64 simulator

--- MTStatusBarOverlayBinding/libMTStatusBarOverlay.linkwith.cs
+++ MTStatusBarOverlayBinding/libMTStatusBarOverlay.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith("libMTStatusBarOverlay.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith("libMTStatusBarOverlay.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]
